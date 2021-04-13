c10402a73a5a965d272cb5187487bbe1500f7938
xmano <mano@xkanda.com>
2021-04-13 19:55:32 +0530

    Added route to landing page

--- apps/marketplace/src/app/app-routing.module.ts
+++ apps/marketplace/src/app/app-routing.module.ts
@@ -1,12 +1,29 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+﻿import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  {
+    path: 'landing',
+    loadChildren: () => import('./landing/landing.component').then(m => m.LandingComponent)
+  },
+  {
+    path: '',
+    redirectTo: 'landing',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [],
+  exports: [RouterModule],
   imports: [
-    CommonModule
+    RouterModule.forRoot(routes, {
+      initialNavigation: 'enabled',
+      anchorScrolling: 'enabled',
+      onSameUrlNavigation: 'reload',
+      paramsInheritanceStrategy: 'always',
+      relativeLinkResolution: 'corrected',
+    })
   ]
 })
 export class AppRoutingModule { }
