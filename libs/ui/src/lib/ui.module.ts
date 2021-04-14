677cf5f30dfd6857b9c82382cdd2322ab146b8a7
xmano <mano@xkanda.com>
2021-04-14 16:00:03 +0530

    Add header navbar and styling

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatButtonModule,
+    MatToolbarModule
+  ],
   declarations: [
     HeaderComponent
   ],
