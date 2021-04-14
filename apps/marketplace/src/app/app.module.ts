b73783d7234baeded02b726b2c7d6105e4bc8aa8
bruce <delorme.bruce.michel@gmail.com>
2021-04-14 11:01:17 +0200

    typo

--- apps/marketplace/src/app/app.module.ts
+++ apps/marketplace/src/app/app.module.ts
@@ -11,7 +11,7 @@ const FIREBASE_EMUTLATORS = environment.useEmulators ? [
 
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
-import { AppRoutingModule } from './app-routing.module';;
+import { AppRoutingModule } from './app-routing.module';
 
 import { UiModule } from '@nft/ui';
 
