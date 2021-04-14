13b7f28f5147a1bbe77731e3f83ee31423a109c0
Remco <RemcoSimonides@gmail.com>
2021-04-14 16:12:39 +0200

    Added your tokens

--- apps/marketplace/src/app/landing/landing-routing.module.ts
+++ apps/marketplace/src/app/landing/landing-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './landing.component';
-import { EthereumWalletGuard } from '@nft/metamask';
 
 const routes: Routes = [
   {
     path: '',
     component: LandingComponent,
-    canActivate: [EthereumWalletGuard],
   }
 ];
 
