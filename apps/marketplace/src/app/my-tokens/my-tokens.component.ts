ea0409ccba3351e81ba0163ebc6cd78e40047f3c
Mussuranae <mandonnetdev@outlook.com>
2021-04-15 11:08:30 +0200

    WIP

--- apps/marketplace/src/app/my-tokens/my-tokens.component.ts
+++ apps/marketplace/src/app/my-tokens/my-tokens.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MetamaskService } from 'libs/metamask/src/lib/+state/metamask.service';
+import { OpenSeaService } from 'libs/metamask/src/lib/+state/open-sea.service';
 
 @Component({
   selector: 'nft-my-tokens',
@@ -9,13 +10,17 @@ import { MetamaskService } from 'libs/metamask/src/lib/+state/metamask.service';
 export class MyTokensComponent implements OnInit {
 
   public loggedIn: undefined | boolean;
+  public item: any;
 
   constructor(
-    public metamaskService: MetamaskService
+    public metamaskService: MetamaskService,
+    private openseaService: OpenSeaService
   ) { }
 
   async ngOnInit() {
-    this.loggedIn = await this.metamaskService.hasAccount()
+    this.loggedIn = await this.metamaskService.hasAccount();
+
+    this.item = this.openseaService.getItem('0xeE45B41D1aC24E9a620169994DEb22739F64f231', '28617087641888108276224327395903793873568828356690862626991544740160519274497')
   }
 
   public async signIn() {
--- libs/metamask/src/lib/+state/open-sea.service.ts
+++ libs/metamask/src/lib/+state/open-sea.service.ts
@@ -1,11 +1,15 @@
 import { Injectable, OnInit } from '@angular/core';
 import { OpenSeaPort, Network } from 'opensea-js'
 import { MetamaskService } from './metamask.service';
-import env from '@nft/env'
+import env from '@nft/env';
+
+
 @Injectable({ providedIn: 'root' })
 export class OpenSeaService implements OnInit {
   seaport!: OpenSeaPort;
-  constructor(private metamask: MetamaskService) { }
+
+  constructor(private metamask: MetamaskService) {
+  }
 
   async ngOnInit() {
     if (this.metamask.signer) {
@@ -14,5 +18,10 @@ export class OpenSeaService implements OnInit {
     this.seaport = new OpenSeaPort(this.metamask.signer, {
       networkName: env.production ? Network.Main : Network.Rinkeby
     })
+  }
+
+  async getItem(address: string, tokenId: string | number | null) {
+    const item = await this.seaport.api.getAsset({ tokenAddress: address, tokenId });
+    return item;
   }
 }
