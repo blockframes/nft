efd6e58c6bbf1f2b8276759c5555c6d7f78a33d5
xmano <mano@xkanda.com>
2021-04-15 17:36:09 +0530

    Add Contacts page

--- apps/marketplace/src/app/contact-us/contact-us.component.ts
+++ apps/marketplace/src/app/contact-us/contact-us.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-us.component.scss']
 })
 export class ContactUsComponent implements OnInit {
-
-  constructor() { }
+  subject: string;
+  message: string;
+  constructor() { 
+    this.subject = "";
+    this.message = "";
+  }
 
   ngOnInit(): void {
   }
 
+  sendMail() {
+    window.open(`mailto:mbangera@cascade8.com?subject=${this.subject}&body=${this.message}`);
+  }
 }
