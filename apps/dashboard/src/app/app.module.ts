b7a659edf510fe6bf1739c8cb1bf25a2fa4ae2b4
samir <samir.touira@epitech.eu>
2021-04-16 13:48:04 +0200

    router outlet + landing route dashboard

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -1,12 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireFunctionsModule } from '@angular/fire/functions';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule, USE_EMULATOR as USE_FIRESTORE_EMULATOR } from '@angular/fire/firestore';
+
+// Specific config for emulators
+const FIREBASE_EMUTLATORS = env.useEmulators ? [
+  { provide: USE_FIRESTORE_EMULATOR, useValue: ['localhost', 8000] },
+] : [];
 
 import { AppComponent } from './app.component';
+import env from '@nft/env';
+import { UiModule } from '@nft/ui';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
-  providers: [],
+  imports: [
+    UiModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(env.firebase),
+    AngularFirestoreModule,
+    AngularFireFunctionsModule,
+    AppRoutingModule
+  ],
+  providers: [...FIREBASE_EMUTLATORS],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
