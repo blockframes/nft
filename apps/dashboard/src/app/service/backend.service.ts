0b85d6285cd38c4062153614c98735fb42ea648a
xmano <mano@xkanda.com>
2021-04-16 19:09:02 +0530

    Add create function to backend service

--- apps/dashboard/src/app/service/backend.service.ts
+++ apps/dashboard/src/app/service/backend.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+
+import { Title, Attribute } from '../../models/title';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BackendService {
+  private dbPath = '/titles';  //path to database table
 
-  constructor() { }
+  titlesRef: AngularFireList<Title>;
+
+  constructor(private db: AngularFireDatabase) { 
+    this.titlesRef = db.list(this.dbPath);
+  }
+
+  create(title: Title): any {
+    return this.titlesRef.push(title);
+  }  
+
 }
--- libs/ui/src/lib/dashboard/dashboard.service.ts
+++ libs/ui/src/lib/dashboard/dashboard.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardService {
+  titles: AngularFireList<any>;
 
-  constructor() { }
+  constructor(private db: AngularFireDatabase) {
+  }
+
+  getTitles(): Observable<any> {
+    this.titles = this.db.list('titles');
+    return this.titles.valueChanges();
+  }
+
 }
