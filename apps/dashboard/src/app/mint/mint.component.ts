80b079bf95fdd16b0b97d10c3e515bea02bf2800
fritzschoff <max.j.f@gmx.de>
2021-04-16 09:06:21 +0200

    added mint component

---
+++ apps/dashboard/src/app/mint/mint.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nft-mint',
+  templateUrl: './mint.component.html',
+  styleUrls: ['./mint.component.scss'],
+})
+export class MintComponent {
+}
