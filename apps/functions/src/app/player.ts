098885395d03b158ecdc4e432066eebbcfd05c71
pldespaigne <pl.despaigne@gmail.com>
2021-04-21 16:01:22 +0200

    play count limitation

--- apps/functions/src/app/player.ts
+++ apps/functions/src/app/player.ts
@@ -1,26 +1,38 @@
 
-import { utils, ethers } from 'ethers';
-import { ERC1155_Meta, SignedMessage, PlayerResponse } from '@nft/model';
+import { utils, getDefaultProvider, Contract } from 'ethers';
+
+import { sign } from 'jsonwebtoken';
 import { createHash } from 'crypto';
-import * as request from 'request-promise';
+import * as admin from 'firebase-admin';
+import { logger, https, config } from 'firebase-functions';
 
 import env from '@nft/env';
 import * as abi from '@nft/model/erc1155.json';
-import { logger, https, config } from 'firebase-functions';
-import { sign } from 'jsonwebtoken';
+import { ERC1155_Meta, SignedMessage, PlayerResponse, TokenLimit } from '@nft/model';
 
 // This variable define the duration (in seconds) of a video link before it expires
 export const linkDuration = 60 * 60 * 5; // 5 hours in seconds = 60 seconds * 60 minutes * 5 = 18 000 seconds
+export const oneDay = 1000 * 60 * 60 * 24; // one day in milliseconds = 1000 milliseconds * 60 seconds * 60 minutes * 24 hours = 86 400 000 milliseconds
 
+export const MAX_PLAY_COUNT = 10;
 
+/** Admin instance of the RealTime Database */
+export const app = admin.initializeApp();
+export const db = app.database();
 
 export const checkSignature = async (data: SignedMessage, context: https.CallableContext): Promise<PlayerResponse> => {
+
+  // ------------------------------
+  // CHECK PRECONDITIONS
 
   const jwplayerSecret = config().jwplayer?.secret;
 
   if (!jwplayerSecret) {
     throw new https.HttpsError('failed-precondition', `jwplayer.secret env variable hasn't been set ! Check functions config in CLI with 'firebase functions:config:get'!`);
   }
+
+  // ------------------------------
+  // COMPUTE PLAYER URL
 
   const expires = Math.floor(new Date().getTime() / 1000) + linkDuration; // now + 5 hours
 
@@ -32,33 +44,59 @@ export const checkSignature = async (data: SignedMessage, context: https.Callabl
   const playerUrl = `https://cdn.jwplayer.com/libraries/3sZQvkmL.js?exp=${expires}&sig=${playerSignature}`;
 
   // ------------------------------
+  // VERIFY OWNERSHIP OF THE TOKEN
 
   const { message, signature, tokenId } = data;
   const ethAddress = utils.verifyMessage(message, signature);
   logger.log(`Ethereum address : ${ethAddress}`, `Token Id: ${tokenId}`);
 
-  const provider = ethers.getDefaultProvider(env.eth.network);
-  const contract = new ethers.Contract(env.eth.erc1155, abi, provider);
+  const provider = getDefaultProvider(env.eth.network);
+  const contract = new Contract(env.eth.erc1155, abi, provider);
 
-  const [balance, uri] = await Promise.all([
-    contract.balanceOf(ethAddress, tokenId),
-    contract.uri(tokenId),
-  ]);
+  const balance = await contract.balanceOf(ethAddress, tokenId);
 
   const amount: number = balance.toNumber();
   if (amount === 0) {
     throw new https.HttpsError('permission-denied', `${ethAddress} does not own token : ${tokenId}`);
   }
 
-  const options = {
-    method: 'GET',
-    url: uri,
-    json: true,
-    headers: { 'User-Agent': 'client' },
-  };
-  const meta: ERC1155_Meta = await request.get(options);
 
-  // Check country
+  // ------------------------------
+  // CHECK TOKEN LIMITS
+
+  const limitsRef = db.ref('owners').child(ethAddress).child(`${tokenId}`);
+  const limitsSnap = await limitsRef.get();
+  const now = Date.now();
+  if (!limitsSnap.exists()) {
+    // create new limits in case this user just acquired the token and watch the movie for the first time
+    await limitsRef.set({ lastView: now, playCount: 1 } as TokenLimit);
+  } else {
+    const limits: TokenLimit = limitsSnap.val();
+    const dateThreshold = limits.lastView + oneDay;
+    if (now > dateThreshold) {
+      if (limits.playCount >= MAX_PLAY_COUNT) {
+        throw new https.HttpsError('permission-denied', `You already have seen this movie ${MAX_PLAY_COUNT} times!`);
+      }
+      limits.lastView = now;
+      limits.playCount += 1;
+      await limitsRef.set(limits);
+    }
+  }
+
+  // ------------------------------
+  // RETRIEVE TOKEN METADATA
+
+  const metaSnap = await db.ref('titles').child(`${tokenId}`).get();
+  if (!metaSnap.exists()) {
+    logger.error(`METADATA NOT FOUND: ${ethAddress} is the legit owner of token ${tokenId}, but this token as no metadata in our RealTime DB !!!!`);
+    throw new https.HttpsError('internal', `You (${ethAddress}) own this token (${tokenId}), but we haven't found its data in our servers. Please contact us!`);
+  }
+  const meta: ERC1155_Meta = metaSnap.val();
+
+
+  // ------------------------------
+  // CHECK USER COUNTRY AGAINST TOKEN ALLOWED COUNTRIES
+
   const req = context.rawRequest;
   const engineCountry = req.headers['x-appengine-country'];
   if (engineCountry && typeof engineCountry === 'string') {
@@ -72,6 +110,9 @@ export const checkSignature = async (data: SignedMessage, context: https.Callabl
     }
   }
 
+  // ------------------------------
+  // COMPUTE VIDEO URL
+
   const resource = `/v2/media/${meta.jwPlayerId}/drm/lDDldMGd`;
 
   const jwt = sign({
@@ -80,6 +121,9 @@ export const checkSignature = async (data: SignedMessage, context: https.Callabl
   }, jwplayerSecret);
 
   const videoUrl = `https://cdn.jwplayer.com${resource}?token=${jwt}`;
+
+  // ------------------------------
+  // RETURN NEEDED DATA
 
   return {
     playerUrl,
