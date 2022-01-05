94cd574a600a27c18029521e43e64580675d863c
pldespaigne <pl.despaigne@gmail.com>
2022-01-05 14:51:08 +0100

    cleaning & refactoring

--- libs/model/src/lib/models.ts
+++ libs/model/src/lib/models.ts
@@ -9,10 +9,3 @@ export interface PlayerResponse {
   videoUrl: string;
   jwPlayerId: string;
 }
-
-export interface TokenLimit {
-  /** Unix timestamp of the date when the last view occurred */
-  lastView: number;
-  /** Number of times this owner has viewed this token's movie */
-  playCount: number;
-}
--- libs/model/src/lib/title.ts
+++ libs/model/src/lib/title.ts
@@ -1,11 +1,21 @@
-export interface Attribute {
-  ['key']: string,
-}
+
+interface OpenSeaStringAttribute {
+  trait_type: string;
+  value: string;
+};
+
+interface TitleAttribute extends OpenSeaStringAttribute {
+  trait_type: 'Title',
+};
+
+type Attribute = TitleAttribute;
 
 export interface Title {
-  attributes: Attribute;
+  attributes?: Attribute[];
   description: string;
   image: string;
-  jwPlayerId: string;
+  jwPlayerId?: string;
   name: string;
-}
+  animation_url?: string;
+  background_color?: string;
+};
